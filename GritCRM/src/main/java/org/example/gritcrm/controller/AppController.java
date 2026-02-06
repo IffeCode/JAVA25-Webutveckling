8465bbeefc22be0439a0da4776a90e97cfecac6c
iffe <iffe.h@hotmail.com>
2026-02-06 09:44:59 +0100

    fixed!

--- GritCRM/src/main/java/org/example/gritcrm/controller/AppController.java
+++ GritCRM/src/main/java/org/example/gritcrm/controller/AppController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -12,8 +13,13 @@ public class AppController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        //super viktigt att ha med getRequestDispatcher för att det ska gå att öppna
-        req.getRequestDispatcher("view/index.jsp").forward(req,resp);
+        HttpSession session = req.getSession(false);
+        if (session == null || session.getAttribute("user") == null) {
+            req.getRequestDispatcher("/view/login.jsp").forward(req, resp);
+        } else {
+            //super viktigt att ha med getRequestDispatcher för att det ska gå att öppna
+            req.getRequestDispatcher("view/index.jsp").forward(req,resp);
+        }
 
 
     }
