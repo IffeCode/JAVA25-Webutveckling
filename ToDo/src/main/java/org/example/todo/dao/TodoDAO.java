76ddcdd82d2aae9a4020a70f32746f75ccabe301
iffe <iffe.h@hotmail.com>
2026-02-19 12:15:31 +0100

    skapat en todo applikation!

--- ToDo/src/main/java/org/example/todo/dao/TodoDAO.java
+++ ToDo/src/main/java/org/example/todo/dao/TodoDAO.java
@@ -6,4 +6,20 @@ public class TodoDAO extends GenericDAO<Todo, Integer>{
     public TodoDAO() {
         super(Todo.class);
     }
+
+    public Todo findById(String sId){
+        if (sId == null || sId.isEmpty()){
+            return null;
+        }
+
+        int id;
+        try {
+            id = Integer.parseInt(sId);
+        }catch (NumberFormatException e){
+            return null;
+        }
+
+        return findById(id);
+    }
+
 }
